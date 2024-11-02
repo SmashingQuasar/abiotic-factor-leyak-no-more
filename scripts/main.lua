abe5445dfd7dcdf32334a01ef7799ef5498abc31
Nicolas Lebacq <nicolas.cordier@outlook.com>
2024-11-02 09:23:34 +0100

    feat: Added permanent containment option and persistent options. (#5)

--- scripts/main.lua
+++ scripts/main.lua
@@ -1,7 +1,11 @@
 require("UEHelpers")
 local utils = require("utils")
 local hooks = require("hooks")
+local configuration = require("configuration")
+
+configuration.load()
 
 hooks.registerMainHook()
+hooks.registerNewDayUpdateHook()
 
 utils.log("Mod loaded.")
